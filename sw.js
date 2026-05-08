ad8feb39212a7dd0f12126e6add5b3fea15877fe
malboushi <mohammad.alaa.love.619@gmail.com>
2026-05-08 20:08:57 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'admin-panel-v1';
+
+// حطينا بس ملف الأدمن والصورة لأن الـ CSS والـ JS موجودين بداخل ملف الـ HTML نفسه
+const urlsToCache = [
+  './admin.html',
+  './icon.png'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
